e1810fa3e7dfe53de7c309acd377becceb0b85d7
riably <devriably@gmail.com>
2016-11-02 12:22:25 +0100

    auth get useres/me

--- server/models/user.js
+++ server/models/user.js
@@ -4,61 +4,70 @@ const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
-  email:{
-    type:String,
-    required:true,
+  email: {
+    type: String,
+    required: true,
+    trim: true,
     minlength: 1,
-    trim:true,
-    unique:true,
-    validate:{
+    unique: true,
+    validate: {
       validator: validator.isEmail,
       message: '{VALUE} is not a valid email'
     }
   },
-  password:{
+  password: {
     type: String,
-    required: true,
-    minlength:6
+    require: true,
+    minlength: 6
   },
-  tokens:[{
+  tokens: [{
     access: {
-      type:String,
-      required:true
+      type: String,
+      required: true
     },
-    token:{
-      type:String,
-      required:true
+    token: {
+      type: String,
+      required: true
     }
   }]
 });
 
-UserSchema.methods.toJSON = function(){
+UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
 
-  return _.pick(userObject, ['_id','email'])
-}
+  return _.pick(userObject, ['_id', 'email']);
+};
 
-UserSchema.methods.generateAuthToken = function(){
+UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token  = jwt.sign({_id:user._id.toHexString(), access},'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
   user.tokens.push({access, token});
-  return user.save().then(()=>{
+
+  return user.save().then(() => {
     return token;
-  })
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
 };
 
 var User = mongoose.model('User', UserSchema);
 
-// var newUser = new User({
-//   email: '  ejemplo@hotmail.com'
-// });
-// newUser.save().then((doc)=>{
-//   console.log('Saved user', doc);
-// },(e)=>{
-//   console.log('Unable to save user',e);
-// });
-
-module.exports={User};
+module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID}=require('mongodb');
 var {mongoogse} =require('./db/mongoose');
 var {Todo} =require('./models/todo');
 var {User} =require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app= express();
 const port = process.env.PORT || 3000;
@@ -85,19 +86,23 @@ app.patch('/todos/:id',(req,res) =>{
   });
 });
 
-app.post('/users',(req,res)=>{
-  var body = _.pick(req.body, ['email','password']);
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-  user.save().then(()=>{
+  user.save().then(() => {
     return user.generateAuthToken();
-  }).then((token)=>{
+  }).then((token) => {
     res.header('x-auth', token).send(user);
-  }).catch((e)=>{
+  }).catch((e) => {
     res.status(400).send(e);
   })
 });
 
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 app.listen(port,()=>{
   console.log(`Started on port ${port}`);
