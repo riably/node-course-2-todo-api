70aa2edbb529d1674be1ca2a89a58d08f4895fc9
riably <devriably@gmail.com>
2016-11-02 08:53:54 +0100

    get by id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID}=require('mongodb');
 var {mongoogse} =require('./db/mongoose');
 var {Todo} =require('./models/todo');
 var {User} =require('./models/user');
@@ -28,6 +29,21 @@ app.get('/todos', (req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   });
+});
+app.get('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  User.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+      return res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send()
+  });
+
 });
 
 app.listen(3000,()=>{
